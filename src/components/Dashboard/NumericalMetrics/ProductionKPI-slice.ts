0be08b5e607d9f168cc66ff36465dfd270e4f4e3
YUVARAJ PANDIYAN S <yuvaraj.p@appening.xyz>
2022-06-26 17:00:53 +0530

    feat: dashboard with chart

--- src/components/Dashboard/NumericalMetrics/ProductionKPI-slice.ts
+++ src/components/Dashboard/NumericalMetrics/ProductionKPI-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { faker } from "@faker-js/faker";
 
 export type productionMetricsValueTypes = "percentage" | "currency" | "number";
